5b0f92cfef2c6c655872823bfe7a0147b6364f1c
Manfredi Soldano <manfredisoldano@outlook.com>
2020-04-24 21:23:43 +0200

    Including error log and new queries
    Included queries for "Pagamenti" and queries for let the website works.

--- Complete-Website/models/Database.js
+++ Complete-Website/models/Database.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var pool  = mysql.createPool({
-    host     : '',
-    port: '',
-    user     : '',
-    password : ''
+    host     : 'localhost',
+    port: '3306',
+    user     : 'root',
+    password : 'YourPassword' 
 });
 
 exports.getConnection = function() {
